1d0b32d7125425faf4722064b1ac9fd5f0481c49
franprogweb98 <franprogweb98@gmail.com>
2022-08-20 20:55:07 -0300

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,24 @@
 import { SessionProvider, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Layout from "../components/Layout";
+import { usePageLoading } from "../hooks/usePageLoading";
 import "../styles/globals.css";
 import { StoreProvider } from "../utils/Store";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const { isPageLoading } = usePageLoading();
 
   return (
     <StoreProvider>
       <SessionProvider session={session}>
         {Component.auth ? (
           <Auth>
-            <Component {...pageProps} />
+            {isPageLoading ? <h1>Loading...</h1> : <Component {...pageProps} />}
           </Auth>
         ) : (
-          <Component {...pageProps} />
+          <>
+            {isPageLoading ? <h1>Loading...</h1> : <Component {...pageProps} />}
+          </>
         )}
       </SessionProvider>
     </StoreProvider>
